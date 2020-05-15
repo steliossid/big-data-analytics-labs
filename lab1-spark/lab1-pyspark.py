5bf70a797dbab47a1688e246d1ba85c41ab584b9
Stylianos Sidiropoulos <stsid95@gmail.com>
2020-05-15 15:23:30 +0300

    lab1 fixes

--- lab1-spark/lab1-pyspark.py
+++ lab1-spark/lab1-pyspark.py
@@ -21,6 +21,8 @@ min_temperatures = year_temperature.reduceByKey(min)
 max_temperatures = max_temperatures.sortBy(ascending = False, keyfunc=lambda k: k[1])
 min_temperatures = min_temperatures.sortBy(ascending = False, keyfunc=lambda k: k[1])
 
+# print(min_temperatures.collect())
+# print(max_temperatures.collect())
 min_temperatures.saveAsTextFile("output/1_min_temps")
 max_temperatures.saveAsTextFile("output/1_max_temps")
 
@@ -31,6 +33,7 @@ date_temperature = date_temperature.filter(lambda x: int(x[0][0:4])>=1950 and in
 count_month_occ = date_temperature.map(lambda x: (x[0], 1))
 count_month_occ = count_month_occ.reduceByKey(lambda a,b:a+b)
 
+# print(count_month_occ.collect())
 count_month_occ.saveAsTextFile("output/2_count_readings")
 
 # distinct readings from each station
@@ -42,6 +45,7 @@ date_temperature = date_temperature.map(lambda x: (x[0], x[1][0])).distinct()
 count_month_occ = date_temperature.map(lambda x: (x[0], 1))
 count_month_occ = count_month_occ.reduceByKey(lambda a,b:a+b)
 
+# print(count_month_occ.collect())
 count_month_occ.saveAsTextFile("output/2_count_distinct_readings")
 
 # 3
@@ -53,13 +57,17 @@ month_temperatures = min_max_temperatures.map((lambda x: ((x[0][0][0:7], x[0][1]
 sum_month_temperatures = month_temperatures.reduceByKey(lambda a,b: a+b)
 avg_month_temperatures = sum_month_temperatures.map(lambda x: ((x[0][0], x[0][1]), round(x[1]/62, 1)))
 
+# print(avg_month_temperatures.collect())
 avg_month_temperatures.saveAsTextFile("output/3_avg_monthly_temp")
 
 # 4
-station_precipitation = prec_lines.map(lambda x: (x[0], float(x[3])))
+station_precipitation = prec_lines.map(lambda x: ((x[0], x[1]), float(x[3])))
 station_temperature = lines.map(lambda x: (x[0], float(x[3])))
 
-station_precipitation = station_precipitation.filter(lambda x: x[1]>=0.1 and x[1]<=0.2)
+station_precipitation = station_precipitation.reduceByKey(lambda a,b: a+b)
+station_precipitation = station_precipitation.map(lambda x: (x[0][0], x[1]))
+
+station_precipitation = station_precipitation.filter(lambda x: x[1]>=100 and x[1]<=200)
 station_temperature = station_temperature.filter(lambda x: x[1]>=25 and x[1]<=30)
 
 station_precipitation = station_precipitation.reduceByKey(max)
@@ -67,7 +75,8 @@ station_temperature = station_temperature.reduceByKey(max)
 
 station_precipitation_temperature = station_precipitation.join(station_temperature)
 
-avg_month_temperatures.saveAsTextFile("output/4_station_precipitation_temperature")
+# print(station_precipitation_temperature.collect())
+station_precipitation_temperature.saveAsTextFile("output/4_station_precipitation_temperature")
 
 # 5
 og_stations = ostergotland_lines.map(lambda x: (x[0])).collect()
@@ -79,14 +88,13 @@ station_precipitation = station_precipitation.filter(lambda x: int(x[0][0][0:4])
                                                      and x[0][1] in og_stations)
 
 total_monthly_prec_for_station = station_precipitation.reduceByKey(lambda x,y: x+y)
+number_of_prec_for_station = station_precipitation.map(lambda x: ((x[0][0], x[0][1]), 1)).reduceByKey(lambda x,y: x+y)
+avg_prec_for_station = total_monthly_prec_for_station.union(number_of_prec_for_station).reduceByKey(lambda x,y: x/y)
 
-sum_of_month = total_monthly_prec_for_station.map(lambda x: (x[0][0], x[1]))
-sum_of_month = sum_of_month.reduceByKey(lambda x,y: x+y)
+sum_of_month_all = avg_prec_for_station.map(lambda x: (x[0][0], x[1])).reduceByKey(lambda x,y: x+y)
+number_of_stations_in_month = avg_prec_for_station.map(lambda x: (x[0][0], 1)).reduceByKey(lambda x,y: x+y)
+avg_prec_all_stations = sum_of_month_all.union(number_of_stations_in_month).reduceByKey(lambda x,y: x/y)
+avg_prec_all_stations = avg_prec_all_stations.map(lambda x: (x[0], round(x[1],1)))
 
-no_stations_month = station_precipitation.map(lambda x: (x[0][0], 1))
-no_stations_month = no_stations_month.reduceByKey(lambda x,y: x+y)
-
-prec_avg = sum_of_month.union(no_stations_month).reduceByKey(lambda x,y: x/y)
-prec_avg = prec_avg.map(lambda x: (x[0], round(x[1],1)))
-
+# print(prec_avg.collect())
 prec_avg.saveAsTextFile("output/5_ostergotland_monthly_avg_prec")
